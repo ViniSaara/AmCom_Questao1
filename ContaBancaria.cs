9d750b9c1c01c4697fabaa7e41ad19bb461266eb
Vinícius Sabino <viniaraujobh2000@gmail.com>
2023-06-30 15:08:14 -0300

    Implementando classe ContaBancaria

--- ContaBancaria.cs
+++ ContaBancaria.cs
@@ -2,8 +2,36 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    public class ContaBancaria {
 
-       
+        private const double TAXA_SAQUE = 3.5;
+
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = depositoInicial;
+        }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo = Saldo - TAXA_SAQUE - quantia;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Questao1 {
     class Program {
         static void Main(string[] args) {
 
-            ContaBancaria conta = new ContaBancaria();//vini
+            ContaBancaria conta;
 
             Console.Write("Entre o número da conta: ");
             int numero = int.Parse(Console.ReadLine());
@@ -16,29 +16,29 @@ namespace Questao1 {
             if (resp == 's' || resp == 'S') {
                 Console.Write("Entre o valor de depósito inicial: ");
                 double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                //vini conta = new ContaBancaria(numero, titular, depositoInicial);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
             }
             else {
-                //vini conta = new ContaBancaria(numero, titular);
+                conta = new ContaBancaria(numero, titular);
             }
 
             Console.WriteLine();
             Console.WriteLine("Dados da conta:");
-            Console.WriteLine(conta);
+            Console.WriteLine($"Conta {conta.Numero}, Titular: {conta.Titular}, Saldo: $ {conta.Saldo.ToString("N2")}");
 
             Console.WriteLine();
             Console.Write("Entre um valor para depósito: ");
             double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //vini conta.Deposito(quantia);
+            conta.Deposito(quantia);
             Console.WriteLine("Dados da conta atualizados:");
-            Console.WriteLine(conta);
+            Console.WriteLine($"Conta {conta.Numero}, Titular: {conta.Titular}, Saldo: $ {conta.Saldo}");
 
             Console.WriteLine();
             Console.Write("Entre um valor para saque: ");
             quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //vini conta.Saque(quantia);
+            conta.Saque(quantia);
             Console.WriteLine("Dados da conta atualizados:");
-            Console.WriteLine(conta);
+            Console.WriteLine($"Conta {conta.Numero}, Titular: {conta.Titular}, Saldo: $ {conta.Saldo}");
 
             /* Output expected:
             Exemplo 1:
